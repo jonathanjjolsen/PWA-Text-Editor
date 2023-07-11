7edae2690af7108832ab1bf20ec59b8804ab57d1
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-11 13:56:20 -0700

    Added CSS and Babel Loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -44,14 +42,34 @@ module.exports = () => {
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.json('assets', 'icons')
           }
-        ]
+        ],
+        fingerprints: false,
+        inject: true,
       })
       
     ],
 
     module: {
       rules: [
-        
+
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        {
+          //Regex checks for files ending in .js or .mjs with the exception of node_modules
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //Using babel compiler so that the program will be usable in older browsers
+          use: {
+            loader: 'babel-loadeer',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-res-spread', '@babel/transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
