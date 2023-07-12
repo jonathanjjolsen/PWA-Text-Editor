fd63715dee495e5f071ec62b93a9ae0c1f7d9514
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-11 18:28:19 -0700

    Massive Bug Fixes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,20 +17,20 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log(result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+//TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log(result);
-  return result;
+  console.log('index', result);
+  return result?.jate;
 };
 
 initdb();
