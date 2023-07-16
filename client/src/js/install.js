40f662fc965609b0a48aca7cd114dccfed8b402a
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-16 01:09:16 -0700

    Fixed code comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,14 +2,12 @@ const butInstall = document.getElementById('buttonInstall');
 
 let deferredPrompt;
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     deferredPrompt = event;
     console.log('Before Install')
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
